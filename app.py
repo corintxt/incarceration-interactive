f9bd593346b86d5079782ae8d3be93134257e63f
Corin Faife <corin.faife@gmail.com>
2019-05-07 14:33:55 -0400

    Added procfile

--- app.py
+++ app.py
@@ -468,4 +468,5 @@ def draw_map():
 
 if __name__ == '__main__':
     app.secret_key=os.urandom(16).hex()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
